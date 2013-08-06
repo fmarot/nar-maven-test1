426bc1566fa7d6d7785323dfbded6b6909b50091
Francois Marot <Francois.Marot@olea-medical.com>
2013-08-06 14:36:18 +0200

    First commit, testing nar-maven-plugin

---
+++ src/main/c/NativeApp.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "$com_teamtter_NativeApp.h"
+
+JNIEXPORT jstring JNICALL Java_${groupId.replace($a, $b)}_NativeApp_sayHello( JNIEnv *env, jobject obj ) {
+	jstring value; /* the return value */
+
+	char buf[40]; /* working buffer (really only need 20 ) */
+
+	sprintf ( buf, "%s", "Hello NAR World in C !" );
+
+	value = (*env)->NewStringUTF( env, buf );
+
+	return value;
+}
---
+++ src/main/java/com/teamtter/NativeApp.java
@@ -0,0 +1,18 @@
+package com.teamtter;
+
+public class NativeApp
+{
+
+	static
+	{
+		NarSystem.loadLibrary();
+	}
+
+	public final native String sayHello();
+
+	public static void main(String[] args)
+	{
+		NativeApp app = new NativeApp();
+		System.out.println(app.sayHello());
+	}
+}
